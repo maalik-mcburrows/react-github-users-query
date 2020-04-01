1bbe4a1765af5ea900bbf25d663e598120d24ea4
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-01 11:50:59 -0400

    app js file complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    userName: "",
+    users: []
+  }
+
+  handleSubmit = async(event) => {
+    event.preventDefault();
+    const username = this.state.userName;
+    const data = await fetch(`https://api.github.com/users/${username}`)
+    const response = data.json();
+    this.setState({
+      userName: "",
+      users: [...this.state.users, response]
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      userName: event.target.value
+    })
+  }
+
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>User Query Exercise</h1>
+        </header>
+        <form onSubmit={this.handleSubmit}>
+          <label> Type a User's Name
+            <input type="text" value={this.state.userName} placeholder="type sum" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
